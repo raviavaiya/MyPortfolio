7b340206ebe4ddc01114c353868e5d7552021ba5
raviavaiya <raviavaiya03@gmail.com>
2024-05-24 11:04:59 +0530

    Portfolio Is Completed.

--- index.php
+++ index.php
@@ -10,16 +10,14 @@
   <meta content="" name="keywords">
 
   <!-- Favicons -->
-  <link href="assets/img/favicon.png" rel="icon">
-  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+  <link href="assets/img/logo.jpg" rel="icon">
+  <link href="assets/img/logo.jpg" rel="apple-touch-icon">
 
   <!-- Google Fonts -->
-  
+
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Satisfy" rel="stylesheet">
 
-  <link
-    href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Satisfy"
-    rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Satisfy" rel="stylesheet">
 
 
   <!-- Vendor CSS Files -->
@@ -31,6 +29,22 @@
 
   <!-- Template Main CSS File -->
   <link href="assets/css/style.css" rel="stylesheet">
+  <script>
+    // Function to hide the message box after a delay
+    function hideMessageBox() {
+      var messageBox = document.querySelector('.sent-message, .error-message');
+      if (messageBox) {
+        setTimeout(function() {
+          messageBox.style.display = 'none';
+        }, 5000); // 5000 milliseconds = 5 seconds
+      }
+    }
+
+    // Call the hideMessageBox function when the page loads
+    window.onload = function() {
+      hideMessageBox();
+    };
+  </script>
 
 </head>
 
@@ -76,7 +90,7 @@
         <div class="row">
           <div class="image col-lg-4 d-flex align-items-stretch justify-content-center justify-content-lg-start"></div>
           <div class="col-lg-8 d-flex flex-column align-items-stretch">
-            <div class="content ps-lg-4 d-flex flex-column justify-content-center">
+            <div class="content ps-lg-4 d-flex flex-column justify-content-center"><br>
               <div class="row">
                 <div class="col-lg-6">
                   <ul>
@@ -108,7 +122,7 @@
 
                     <span data-purecounter-start="0" data-purecounter-end="22" data-purecounter-duration="1" class="purecounter"></span>
 
-                   
+
 
                     <p><strong>Happy Clients</strong> consequuntur voluptas nostrum aliquid ipsam architecto ut.</p>
                   </div>
@@ -120,7 +134,7 @@
 
                     <span data-purecounter-start="0" data-purecounter-end="35" data-purecounter-duration="1" class="purecounter"></span>
 
-                   
+
 
                     <p><strong>Projects</strong> adipisci atque cum quia aspernatur totam laudantium et quia dere tan
                     </p>
@@ -132,7 +146,7 @@
 
                     <span data-purecounter-start="0" data-purecounter-end="2" data-purecounter-duration="1" class="purecounter"></span>
 
-                    
+
 
                     <p><strong>Years of experience</strong> aut commodi quaerat modi aliquam nam ducimus aut voluptate
                       non vel</p>
@@ -145,7 +159,7 @@
 
                     <span data-purecounter-start="0" data-purecounter-end="5" data-purecounter-duration="1" class="purecounter"></span>
 
-                    
+
 
                     <p><strong>Awards</strong> rerum asperiores dolor alias quo reprehenderit eum et nemo pad der</p>
                   </div>
@@ -446,11 +460,8 @@
                 <a href="assets/img/portfolio/portfolio-1.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link" title="Footwear Shop Management System - Asp.Net"><i class="bx bx-plus"></i></a>
                 <a href="https://github.com/raviavaiya/" target="_blank" class="details-link" title="More Details"><i class="bx bxl-github"></i></a>
 
-                <a href="assets/img/portfolio/portfolio-1.jpg" data-gallery="portfolioGallery"
-                  class="portfolio-lightbox preview-link" title="Footwear Shop Management System - Asp.Net"><i
-                    class="bx bx-plus"></i></a>
-                <a href="https://github.com/raviavaiya/" target="_blank" class="details-link" title="More Details"><i
-                    class="bx bxl-github"></i></a>
+                <a href="assets/img/portfolio/portfolio-1.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link" title="Footwear Shop Management System - Asp.Net"><i class="bx bx-plus"></i></a>
+                <a href="https://github.com/raviavaiya/" target="_blank" class="details-link" title="More Details"><i class="bx bxl-github"></i></a>
 
               </div>
             </div>
@@ -467,11 +478,8 @@
                 <a href="assets/img/portfolio/portfolio-10.webp" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link" title="Footwear Shop Management System - PHP"><i class="bx bx-plus"></i></a>
                 <a href="https://github.com/raviavaiya/" target="_blank" class="details-link" title="More Details"><i class="bx bxl-github"></i></a>
 
-                <a href="assets/img/portfolio/portfolio-10.webp" data-gallery="portfolioGallery"
-                  class="portfolio-lightbox preview-link" title="Footwear Shop Management System - PHP"><i
-                    class="bx bx-plus"></i></a>
-                <a href="https://github.com/raviavaiya/" target="_blank" class="details-link" title="More Details"><i
-                    class="bx bxl-github"></i></a>
+                <a href="assets/img/portfolio/portfolio-10.webp" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link" title="Footwear Shop Management System - PHP"><i class="bx bx-plus"></i></a>
+                <a href="https://github.com/raviavaiya/" target="_blank" class="details-link" title="More Details"><i class="bx bxl-github"></i></a>
 
               </div>
             </div>
@@ -521,7 +529,7 @@
                     <h3>Email Me</h3>
                   </a>
 
-               
+
 
                   <p>raviavaiya03@gmail.com</p>
                 </div>
@@ -536,8 +544,8 @@
                   <p>+91 8799559020</p>
 
 
-                  
-              
+
+
 
                 </div>
               </div>
@@ -549,37 +557,37 @@
 
             <form action="index.php" method="post" role="form" class="php-email-form">
 
-            <form action="assets/vendor/php-email-form/mail_config.php" method="post" role="form" class="php-email-form">
+              <form action="assets/vendor/php-email-form/mail_config.php" method="post" role="form" class="php-email-form">
 
-              <div class="row">
-                <div class="col-md-6 form-group">
-                  <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" required>
+                <div class="row">
+                  <div class="col-md-6 form-group">
+                    <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" required>
+                  </div>
+                  <div class="col-md-6 form-group mt-3 mt-md-0">
+                    <input type="email" class="form-control" name="email" id="email" placeholder="Your Email" required>
+                  </div>
                 </div>
-                <div class="col-md-6 form-group mt-3 mt-md-0">
-                  <input type="email" class="form-control" name="email" id="email" placeholder="Your Email" required>
+                <div class="form-group mt-3">
+                  <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" required>
                 </div>
-              </div>
-              <div class="form-group mt-3">
-                <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" required>
-              </div>
-              <div class="form-group mt-3">
+                <div class="form-group mt-3">
 
-                <textarea class="form-control" name="message" id="message" rows="6" placeholder="Message" required></textarea>
+                  <textarea class="form-control" name="message" id="message" rows="6" placeholder="Message" required></textarea>
 
-                
 
-              </div>
-              <div class="my-3">
-                <div class="loading">Loading</div>
-                <div class="error-message"></div>
-                <div class="sent-message">Your message has been sent. Thank you!</div>
-              </div>
 
-              <div class="text-center"><button type="submit" name="send">Send Message</button></div>
+                </div>
+                <div class="my-3">
+                  <div class="loading">Loading</div>
+                  <div class="error-message"></div>
+                  <div class="sent-message">Your message has been sent. Thank you!</div>
+                </div>
 
-             
+                <div class="text-center"><button type="submit" name="send">Send Message</button></div>
 
-            </form>
+
+
+              </form>
           </div>
 
         </div>
@@ -615,8 +623,7 @@
 
   <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
-  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
-      class="bi bi-arrow-up-short"></i></a>
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
 
   <!-- Vendor JS Files -->
@@ -627,9 +634,14 @@
   <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
   <script src="assets/vendor/waypoints/noframework.waypoints.js"></script>
   <script src="assets/vendor/php-email-form/validate.js"></script>
+  <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/toastify-js/src/toastify.min.css">
+  <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/toastify-js"></script>
 
   <!-- Template Main JS File -->
   <script src="assets/js/main.js"></script>
+
+
+
 
 </body>
 
@@ -660,7 +672,7 @@ if (isset($_POST['send'])) {
 
   try {
     //Server settings
-                       //Enable verbose debug output
+    //Enable verbose debug output
     $mail->isSMTP();                                            //Send using SMTP
     $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
@@ -680,11 +692,43 @@ if (isset($_POST['send'])) {
     $mail->Body    = "<h4>Email From : <b> {$email} </b></h4><br><h3>Subject :<b>{$subject}</b></h3><br><h4>Full Name : <b>{$name}</b></h4><br><h5>Message :<b>{$message}</b></h5>";
 
     $mail->send();
-    echo 'Message has been sent';
+    // echo "<script>window.location.hash = '#contact';</script>";
+    //   echo "<script>
+    //   document.querySelector('.sent-message').style.display = 'block';
+    //   document.querySelector('.sent-message').innerText = 'Your message has been sent. Thank you!';
+    // </script>";
+    echo '<script>Toastify({
+    text: "Your message has been sent. Thank you!",
+    duration: 3000,
+    newWindow: true,
+    close: true,
+    gravity: "top", // `top` or `bottom`
+    position: "right", // `left`, `center` or `right`
+    stopOnFocus: true, // Prevents dismissing of toast on hover
+    style: {
+      background: "linear-gradient(to right, #00b09b, #96c93d)",
+    },
+    onClick: function(){} // Callback after click
+  }).showToast();</script>';
   } catch (Exception $e) {
-    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+    //   echo "<script>
+    //   document.querySelector('.error-message').style.display = 'block';
+    //   document.querySelector('.error-message').innerText = 'Message could not be sent. Mailer Error: {$mail->ErrorInfo}';
+    // </script>";
+    echo '<script>Toastify({
+    text: "Message could not be sent. Mailer Error: {$mail->ErrorInfo}",
+    duration: 3000,
+    newWindow: true,
+    close: true,
+    gravity: "top", // `top` or `bottom`
+    position: "right", // `left`, `center` or `right`
+    stopOnFocus: true, // Prevents dismissing of toast on hover
+    style: {
+      background: "linear-gradient(to right, #00b09b, #96c93d)",
+    },
+    onClick: function(){} // Callback after click
+  }).showToast();</script>';
   }
 }
 
 ?>
-
